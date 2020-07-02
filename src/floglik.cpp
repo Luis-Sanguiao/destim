a8fc8874a19e37a351d0d01fbb90f1dd275036ff
Luis Sanguiao Sande <luis.sanguiao.sande@gmail.com>
2020-07-02 14:12:22 +0200

    fmultiloglik added (not tested)

--- src/floglik.cpp
+++ src/floglik.cpp
@@ -48,3 +48,46 @@ double floglik(SEXP & TM, Eigen::Map<Eigen::VectorXd> values, const Eigen::Map<E
 
   return(-output);
 }
+
+// [[Rcpp::export]]
+double fmultiloglik(SEXP & TM, Eigen::Map<Eigen::VectorXd> values, const Eigen::Map<Eigen::VectorXd> rparams,
+               const Eigen::Map<Eigen::MatrixXd> transmatrix, const SEXP & init,
+               const SEXP & EM, const IntegerMatrix & obs) {
+  MSM tmat(as<MSM> (TM));
+  const MSM2 emat(as<MSM2> (EM));
+  const MSM2 smatinit(as<MSM2> (init));
+  SparseMatrix<double> smat(smatinit.rows(), smatinit.cols());
+  VectorXd isum;
+
+  double output = 0;
+  int i = 0, j;
+
+  for (j = 0; j < obs.nrow(); ++j) {
+    if (IntegerVector::is_na(obs(j, i)))
+      smat.col(j) = VectorXd::Ones(smat.rows()).cwiseProduct(smatinit.col(j));
+    else
+      smat.col(j) = emat.col(obs(j,i)).cwiseProduct(smatinit.col(j));
+  }
+
+  values = transmatrix.rightCols(1) + transmatrix.leftCols(rparams.size()) * rparams ;
+
+  isum = VectorXd::Ones(smat.cols()) * smat;
+  output += isum.log().sum();
+  for (j = 0; j < obs.nrow(); ++j)
+    smat.col(j) = smat.col(j) / isum(j);
+
+  for (++i; i < obs.ncol(); ++i) {
+    smat = tmat.transpose() * smat;
+    for (j = 0; j < obs.nrow(); ++j)
+      if (!IntegerVector::is_na(obs(j, i)))
+        smat.col(j) = emat.col(obs(j, i)).cwiseProduct(emat.col(j));
+
+    isum = VectorXd::Ones(smat.cols()) * smat;
+    output += isum.log().sum();
+    for (j = 0; j < obs.nrow(); ++j)
+      smat.col(j) = smat.col(j) / isum(j);
+  }
+
+  return(-output);
+}
+
